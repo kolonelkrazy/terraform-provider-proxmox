3935882ecd9bd5723b2d5c671e7568b7a8c18b24
Josh B <201935441+kolonelkrazy@users.noreply.github.com>
2025-05-09 15:08:14 +0000

    Update fwprovider/access/resource_realm_model.go
    https://github.com/kolonelkrazy/terraform-provider-proxmox/pull/1#discussion_r2081437169
    
    Co-authored-by: Pavel Boldyrev <627562+bpg@users.noreply.github.com>
    Signed-off-by: Josh B <201935441+kolonelkrazy@users.noreply.github.com>

--- fwprovider/access/resource_realm_model.go
+++ fwprovider/access/resource_realm_model.go
@@ -16,42 +16,42 @@ type realmResourceModel struct {
 	Realm types.String `tfsdk:"realm"`
 	Type  types.String `tfsdk:"type"`
 
-	Acrvalues           types.String `tfsdk:"acr-values"`
-	Autocreate          bool         `tfsdk:"autocreate"`
-	Base_dn             types.String `tfsdk:"base_dn"`
-	Bind_dn             types.String `tfsdk:"bind_dn"`
-	Capath              types.String `tfsdk:"capath"`
-	Casesensitive       bool         `tfsdk:"case-sensitive"`
+	AcrValues           types.String `tfsdk:"acr-values"`
+	AutoCreate          bool         `tfsdk:"autocreate"`
+	BaseDn              types.String `tfsdk:"base_dn"`
+	BindDn              types.String `tfsdk:"bind_dn"`
+	CaPath              types.String `tfsdk:"capath"`
+	CaseSensitive       bool         `tfsdk:"case-sensitive"`
 	Cert                types.String `tfsdk:"cert"`
-	Certkey             types.String `tfsdk:"certkey"`
-	Checkconnection     bool         `tfsdk:"check-connection"`
-	Clientid            types.String `tfsdk:"client-id"`
-	Clientkey           types.String `tfsdk:"client-key"`
+	CertKey             types.String `tfsdk:"certkey"`
+	CheckConnection     bool         `tfsdk:"check-connection"`
+	ClientId            types.String `tfsdk:"client-id"`
+	ClientKey           types.String `tfsdk:"client-key"`
 	Comment             types.String `tfsdk:"comment"`
 	Default             bool         `tfsdk:"default"`
 	Domain              types.String `tfsdk:"domain"`
 	Filter              types.String `tfsdk:"filter"`
-	Group_classes       types.String `tfsdk:"group_classes"`
-	Group_dn            types.String `tfsdk:"group_dn"`
-	Group_filter        types.String `tfsdk:"group_filter"`
-	Group_name_attr     types.String `tfsdk:"group_name_attr"`
-	Issuerurl           types.String `tfsdk:"issuer-url"`
+	GroupClasses        types.String `tfsdk:"group_classes"`
+	GroupDn             types.String `tfsdk:"group_dn"`
+	GroupFilter         types.String `tfsdk:"group_filter"`
+	GroupNameAttr       types.String `tfsdk:"group_name_attr"`
+	IssuerUrl           types.String `tfsdk:"issuer-url"`
 	Mode                types.String `tfsdk:"mode"`
 	Password            types.String `tfsdk:"password"`
 	Port                int          `tfsdk:"port"`
-	Prompt              types.String `tfsdk:"prompty"`
+	Prompt              types.String `tfsdk:"promptly"`
 	Scopes              types.String `tfsdk:"scopes"`
 	Secure              bool         `tfsdk:"secure"`
 	Server1             types.String `tfsdk:"server1"`
 	Server2             types.String `tfsdk:"server2"`
-	Sslversion          types.String `tfsdk:"sslversion"`
-	Syncdefaultsoptions types.String `tfsdk:"sync-defaults-options"`
-	Sync_attributes     types.String `tfsdk:"sync_attributes"`
+	SslVersion          types.String `tfsdk:"sslversion"`
+	SyncDefaultsOptions types.String `tfsdk:"sync-defaults-options"`
+	SyncAttributes      types.String `tfsdk:"sync_attributes"`
 	Tfa                 types.String `tfsdk:"tfa"`
-	User_attr           types.String `tfsdk:"user_attr"`
-	User_classes        types.String `tfsdk:"user_classes"`
-	Usernameclaim       types.String `tfsdk:"username-claim"`
-	Verify              bool         `tfsdk:"verify"`
+	UserAttr            types.String `tfsdk:"user_attr"`
+	UserClasses         types.String `tfsdk:"user_classes"`
+	UserNameClaim       types.String `tfsdk:"username-claim"`
+	Verify              bool         `tfsdk:"verify"
 }
 
 const RealmPromptFormat = `(?:none|login|consent|select_account|\S+)`
